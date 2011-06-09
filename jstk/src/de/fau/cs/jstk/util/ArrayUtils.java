7ce3fb6c21196724efb4050d71a6d5b78702accd
florian.hoenig@gmail.com <florian.hoenig@gmail.com@0aadf7e5-db5c-ec0e-a231-36ad200ead0a>
2011-06-09 10:19:47 +0000

    comments

--- jstk/src/de/fau/cs/jstk/util/ArrayUtils.java
+++ jstk/src/de/fau/cs/jstk/util/ArrayUtils.java
@@ -2,12 +2,18 @@ package de.fau.cs.jstk.util;
 
 
 public class ArrayUtils {
-	// an interface for classes that have a public clone() method
+	/**
+	 * an interface for classes that have a public clone() method
+	 * @author hoenig
+	 */
 	public interface PubliclyCloneable {
 		public PubliclyCloneable clone();
 	}
 
 	@SuppressWarnings("unchecked")
+	/**
+	 * clone an array of PubliclyCloneable implementers by calling clone() for each element 
+	 */
 	public static <T extends PubliclyCloneable> T[] arrayClone(T[] orig){
 		// shallow copy:
 		T [] ret = orig.clone();
