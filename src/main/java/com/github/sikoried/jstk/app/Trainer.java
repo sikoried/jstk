46899cbb783b1bc9746a11da0cce7f9d52131da9
Korbinian Riedhammer <korbinianr@gmail.com>
2018-04-19 22:11:00 +0200

    Removed spurious import

--- src/main/java/com/github/sikoried/jstk/app/Trainer.java
+++ src/main/java/com/github/sikoried/jstk/app/Trainer.java
@@ -44,7 +44,6 @@ import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.core.config.Configurator;
-import sun.security.krb5.Config;
 
 
 public class Trainer {
